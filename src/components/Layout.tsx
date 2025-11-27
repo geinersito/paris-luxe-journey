cbf459f1ff43c749f2b049cda9304de9c447f3de
geinersito <geinersito@gmail.com>
2025-11-27 12:47:48 +0100

    feat: WhatsApp flotante click-to-chat - elemento de confianza #1

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import FloatingWhatsApp from './FloatingWhatsApp';
 
 export function Layout() {
   return (
@@ -11,6 +12,7 @@ export function Layout() {
         <Outlet />
       </main>
       <Footer />
+      <FloatingWhatsApp />
     </div>
   );
 }
